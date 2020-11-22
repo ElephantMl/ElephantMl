66fabf76bae2f9d963efff4025830deea1650e01
Полищук Дмитрий <polishuk1003@gmail.com>
2020-11-22 14:58:03 +0300

    add interface classification and regression

---
+++ src/Regression/Regression.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+interface Regression
+{
+    /**
+     * @param array $sample
+     * @param array $labels
+     *
+     * TODO: add exception.
+     */
+    public function fit(array $sample, array $labels): void;
+
+    /**
+     * @param array $sample
+     *
+     * @return int|float
+     *
+     * TODO: add exception.
+     */
+    public function predict(array $sample): int | float;
+}
